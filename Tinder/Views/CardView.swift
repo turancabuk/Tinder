c6211c4d295b9755dad2dcb25c123b870c6d82e3
Turan Çabuk <turan6677@gmail.com>
2024-01-29 11:33:58 +0300

    CardView has been created.

---
+++ Tinder/Views/CardView.swift
@@ -0,0 +1,53 @@
+//
+//  CardView.swift
+//  Tinder
+//
+//  Created by Turan Çabuk on 29.01.2024.
+//
+
+import UIKit
+
+class CardView: UIView {
+    
+    let imageView = UIImageView(image: #imageLiteral(resourceName: "lady5c"))
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        
+        addSubview(imageView)
+        imageView.fillSuperview()
+        
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
+        addGestureRecognizer(panGesture)
+    }
+    @objc fileprivate func handlePan(gesture: UIPanGestureRecognizer) {
+        
+        switch gesture.state {
+        case .changed:
+            handleChanged(gesture)
+        case .ended:
+            handleEnded()
+        default:
+            ()
+        }
+    }
+    fileprivate func handleChanged(_ gesture: UIPanGestureRecognizer) {
+        let translation = gesture.translation(in: nil)
+        self.transform = CGAffineTransform(translationX: translation.x, y: translation.y)
+    }
+    
+    fileprivate func handleEnded() {
+        
+        UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.1, options: .curveEaseOut, animations: {
+            self.transform = .identity
+        }) { (_) in
+            
+        }
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
