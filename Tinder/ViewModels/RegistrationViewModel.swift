668e52738663da5fc5405bc62911ee88a932d0ad
Turan Çabuk <turan6677@gmail.com>
2024-02-02 16:23:54 +0300

    Firebase Database config.

--- Tinder/ViewModels/RegistrationViewModel.swift
+++ Tinder/ViewModels/RegistrationViewModel.swift
@@ -8,38 +8,70 @@
 import UIKit
 import Firebase
 import FirebaseStorage
-import JGProgressHUD
 
 class RegistrationViewModel {
     
     var bindableIsRegistering = Bindable<Bool>()
     var bindableImage = Bindable<UIImage>()
+    
     var fullName: String? {didSet{checkFormValidity()}}
     var email: String? {didSet{checkFormValidity()}}
     var password: String? {didSet{checkFormValidity()}}
     
     func performRegistration(completion: @escaping (Error?) -> ()) {
-        
-        guard let email = self.email, let password = self.password else {return}
+      
+        guard let email = email, let password = password else { return }
         bindableIsRegistering.value = true
-        Auth.auth().createUser(withEmail: email, password: password) { (auth, err) in
+        
+        Auth.auth().createUser(withEmail: email, password: password) { (res, err) in
+            if let err = err {
+                completion(err)
+                return
+            }
+            print("Successfully registered user:", res?.user.uid ?? "")
+            self.saveImageToFirebase(completion: completion)
+        }
+    }
+    fileprivate func saveImageToFirebase(completion: @escaping (Error?) ->()) {
+        
+        let filename = UUID().uuidString
+        let ref = Storage.storage().reference(withPath: "/images/\(filename)")
+        let imageData = self.bindableImage.value?.jpegData(compressionQuality: 0.75) ?? Data()
+        
+        ref.putData(imageData, metadata: nil, completion: { (_, err) in
+            
             if let err = err {
                 completion(err)
                 return
             }
             
-            let fileName = UUID().uuidString 
-            let ref = Storage.storage().reference(withPath: "/images/\(fileName)")
-            let imageData = self.bindableImage.value?.jpegData(compressionQuality: 0.75) ?? Data()
-            ref.putData(imageData, metadata: nil) { (_, err) in
-                
+            print("Finished uploading image to storage")
+            ref.downloadURL(completion: { (url, err) in
                 if let err = err {
                     completion(err)
                     return
-                }else{
-                    print("Finished uploading image to storage")
                 }
+                
+                self.bindableIsRegistering.value = false
+                print("Download url of our image is:", url?.absoluteString ?? "")
+                
+                let imageUrl = url?.absoluteString ?? ""
+                self.saveInfoToFirestore(imageUrl: imageUrl, completion: completion)
+            })
+            
+        })
+    }
+    
+    fileprivate func saveInfoToFirestore(imageUrl: String, completion: @escaping (Error?) -> ()) {
+        let uid = Auth.auth().currentUser?.uid ?? ""
+        let docData = ["fullName": fullName ?? "", "uid": uid, "imageUrl1": imageUrl]
+        Firestore.firestore().collection("users").document(uid).setData(docData) { (err) in
+            if let err = err {
+                completion(err)
+                return
             }
+            
+            completion(nil)
         }
     }
     func checkFormValidity() {
