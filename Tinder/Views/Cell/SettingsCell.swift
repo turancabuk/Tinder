70e2da72448c8b045bb62862a845ff9b36bfe73f
Turan Çabuk <turan6677@gmail.com>
2024-02-06 11:54:06 +0300

    SettingsCell has been created.

---
+++ Tinder/Views/Cell/SettingsCell.swift
@@ -0,0 +1,23 @@
+//
+//  SettingsCell.swift
+//  Tinder
+//
+//  Created by Turan Çabuk on 6.02.2024.
+//
+
+import UIKit
+
+class SettingsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
