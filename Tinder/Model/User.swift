3795c4689ee82770be763f22159e7714e53b68c4
Turan Çabuk <turan6677@gmail.com>
2024-02-09 10:34:12 +0300

    User imagePhoto2, imagePhoto3 has been created in firestore.

--- Tinder/Model/User.swift
+++ Tinder/Model/User.swift
@@ -13,6 +13,8 @@ struct User: ProducesCardViewModel {
     var age: Int?
     var profession: String?
     var imageUrl1: String?
+    var imageUrl2: String?
+    var imageUrl3: String?
     var uid: String?
     
     init(dictionary: [String: Any]) {
@@ -20,7 +22,9 @@ struct User: ProducesCardViewModel {
         self.name = dictionary["fullName"] as? String ?? ""
         self.age = dictionary["age"] as? Int
         self.profession = dictionary["profession"] as? String 
-        self.imageUrl1 = dictionary["imageUrl1"] as? String ?? ""
+        self.imageUrl1 = dictionary["imageUrl1"] as? String 
+        self.imageUrl2 = dictionary["imageUrl2"] as? String
+        self.imageUrl3 = dictionary["imageUrl3"] as? String
         self.uid = dictionary["uid"] as? String ?? ""
     }
     func toCardViewModel() -> CardViewModel {
@@ -29,6 +33,11 @@ struct User: ProducesCardViewModel {
         attributedText.append(NSAttributedString(string: "  \(ageString)", attributes: [.font: UIFont.systemFont(ofSize: 24, weight: .regular)]))
         let professionString = profession != nil ? profession! : "Not available"
         attributedText.append(NSAttributedString(string: "\n\(professionString)", attributes: [.font: UIFont.systemFont(ofSize: 20, weight: .regular)]))
-        return CardViewModel(imageNames: [imageUrl1 ?? ""], attributedString: attributedText, textAlignment: .left)
+        
+        var imageUrls = [String]()
+        if let url = imageUrl1 { imageUrls.append(url)}
+        if let url = imageUrl2 { imageUrls.append(url)}
+        if let url = imageUrl3 { imageUrls.append(url)}
+        return CardViewModel(imageNames: imageUrls, attributedString: attributedText, textAlignment: .left)
     }
 }
